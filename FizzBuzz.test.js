4db186644c168dfaa93b8dba7a1c6e0a2d62a6a0
Lorenatacal <Lorena.tacal@gmail.com>
2018-08-05 22:44:33 +0100

    Test IsDivisibleByThree returns false if number is not divisible by 3

--- FizzBuzz.test.js
+++ FizzBuzz.test.js
@@ -3,3 +3,7 @@ const IsDivisibleByThree = require('./FizzBuzz');
 test("IsDivisibleByThree() should return true when a number argument is divisible by 3", () => {
     expect(IsDivisibleByThree(3)).toEqual(true);
 });
+
+test("IsDivisibleByThree() should return false when a number argument is not divisible by 3", () => {
+    expect(IsDivisibleByThree(4)).toEqual(false);
+});
