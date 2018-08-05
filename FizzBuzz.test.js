7564ca9957c5518869374bbdd2e92a7b7763e427
Lorenatacal <Lorena.tacal@gmail.com>
2018-08-05 22:10:41 +0100

    Correct the testto take one argument instead of 2

--- FizzBuzz.test.js
+++ FizzBuzz.test.js
@@ -1,3 +1,3 @@
 test("IsDivisibleByThree() should return true when a number argument is divisible by 3", () => {
-    expect(IsDivisibleByThree(3,3)).toEqual(true);
+    expect(IsDivisibleByThree(3)).toEqual(true);
 });
