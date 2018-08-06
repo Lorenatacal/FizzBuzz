640ed94d57f09b63f13de7d67f059bbe280b9067
Lorenatacal <Lorena.tacal@gmail.com>
2018-08-06 10:15:29 +0100

    Add the test for a function that will return true if a number argument is divisible by 5

--- FizzBuzz.test.js
+++ FizzBuzz.test.js
@@ -7,3 +7,7 @@ test("IsDivisibleByThree() should return true when a number argument is divisibl
 test("IsDivisibleByThree() should return false when a number argument is not divisible by 3", () => {
     expect(IsDivisibleByThree(4)).toEqual(false);
 });
+
+test("IsDivisibleByFive() should return true when a number argument is divisible by 5", () => {
+    expect(IsDivisibleByFive(5)).toEqual(true);
+});
