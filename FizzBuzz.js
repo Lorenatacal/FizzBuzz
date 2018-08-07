4f1bbd057b5c2db7d85e9397299ef39d3e391252
Lorenatacal <Lorena.tacal@gmail.com>
2018-08-07 10:24:46 +0100

    Make the test pass

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,8 +1,11 @@
 
 const isDivisibleByThree = n => (n % 3 === 0)
 
-function isDivisibleByFive() {
-    return true;
+function isDivisibleByFive(n) {
+    if(n % 5 === 0) {
+        return true;
+    }
+    return false;
 }
 
 module.exports = {
