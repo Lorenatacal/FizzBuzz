53e72efa51765875bad4e8b6ff17271a3b966b05
Lorenatacal <Lorena.tacal@gmail.com>
2018-08-07 10:45:24 +0100

    Refactor the code

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -3,12 +3,7 @@ const isDivisibleByThree = n => (n % 3 === 0)
 
 const isDivisibleByFive = n => (n % 5 === 0)
 
-function isDivisibleByThreeAndFive(n) {
-    if (n % 3 === 0 & n % 5 === 0) {
-        return true;
-    }
-    return false;
-}
+const isDivisibleByThreeAndFive = n => (n % 3 === 0 && n % 5 === 0)
 
 
 module.exports = {
